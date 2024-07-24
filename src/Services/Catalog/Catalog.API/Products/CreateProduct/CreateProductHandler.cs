d6d90af5bcc6cc08d0652b5514012c61af926be0
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2024-07-24 13:15:07 +0530

    Catalog.API - init

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> category, string Description, string ImageFile, decimal Price);
+    public record CreateProductResult(Guid Id);
+    public class CreateProductHandler
+    {
+
+    }
+}
