1f8396d2641603aeba8e49993724e857179e112a
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2024-07-25 11:56:50 +0530

    CQRS - init

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,7 @@
 ﻿namespace Catalog.API.Products.CreateProduct
 {
+    public record CreateProductRequest(string Name, List<string> category, string Description, string ImageFile, decimal Price);
+    public record CreateProductResponse(Guid Id);
     public class CreateProductEndpoint
     {
     }
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,9 +1,35 @@
-﻿namespace Catalog.API.Products.CreateProduct
-{
-    public record CreateProductCommand(string Name, List<string> category, string Description, string ImageFile, decimal Price);
-    public record CreateProductResult(Guid Id);
-    public class CreateProductHandler
-    {
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
 
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // Business logic to create a product
+
+            // create Product entity from command object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+            
+            
+            // save to database
+
+            // return CreateProductResult result
+            return new CreateProductResult(Guid.NewGuid());
+        }
     }
 }
+
+
