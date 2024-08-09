dd09a6f808fdcf6cb57d22cf0c4e298e3f9ffd13
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2024-08-09 12:03:00 +0530

    BuildingBlocks: FluentValidation implemented

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -15,7 +15,7 @@
         }
     }
 
-    internal class CreateProductHandler(IDocumentSession session, IValidator<CreateProductCommand> validator)
+    internal class CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommandHandler> logger)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -23,13 +23,7 @@
             // Business logic to create a product
 
             // create Product entity from command object
-
-            var result = await validator.ValidateAsync(command, cancellationToken);
-            var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
-            if (errors.Any())
-            {
-                throw new ValidationException(errors.FirstOrDefault());
-            }
+            logger.LogInformation("CreateProductCommandHandler.Handle called with {@command}", command);
             var product = new Product
             {
                 Name = command.Name,
