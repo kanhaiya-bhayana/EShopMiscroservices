c1716e9dbb5f4e67d232734bea7917281d2001e4
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2024-08-27 11:46:52 +0530

    BuildingBlocks Handling Exceptions Generic Way with app.UseExceptionHandler

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -22,7 +22,7 @@ namespace Catalog.API.Products.DeleteProduct
             var product = await session.LoadAsync<Product>(command.Id,cancellationToken);
             if (product is null)
             {
-                throw new ProductNotFoundException();
+                throw new ProductNotFoundException(command.Id);
             }
 
             session.Delete<Product>(command.Id);
