d0132355433e38ab043b40c8e7345e37a02d04a4
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2024-09-29 00:28:43 +0530

    Basket.API: GetBasket - init

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿namespace Basket.API.Models;
+public class ShoppingCart
+{
+    public String UserName { get; set; } = default!;
+    public List<ShopingCartItem> Items { get; set; } = new();
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+
+    // Reqruired for mapping
+    public ShoppingCart()
+    {
+        
+    }
+}
+
