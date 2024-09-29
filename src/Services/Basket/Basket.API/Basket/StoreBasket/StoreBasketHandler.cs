1c55d948f424ceea2eeb44c41544d8afe9cd55b1
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2024-09-29 15:30:25 +0530

    Basket.API: Base Structure added

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,27 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
 {
-    public class StoreBasketHandler
+    public StoreBasketCommandValidator()
     {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+        RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class StoreBasketCommandHandler 
+    : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart cart = command.Cart;
+
+        // TODO: store basket in database (use Marten upsert - if exist = update, if not then insert)
+        // TODO: update cache
+
+        return new StoreBasketResult("swn");
     }
 }
