a729a11e94964a20b06b7d3873708f23c8d19d0b
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2024-11-06 11:34:22 +0530

    Discount.GRPC - DiscountService added

---
+++ src/Services/Discount/Discount.Grpc/Migrations/20241106055157_ColumnNameFixed.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Discount.Grpc.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColumnNameFixed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductDescription",
+                table: "Coupons",
+                newName: "Description");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Coupons",
+                newName: "ProductDescription");
+        }
+    }
+}
