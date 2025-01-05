b11fafad4d29c046c5ec9478420b06254670d61e
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2025-01-05 15:25:09 +0530

    Order.Application: DbContext added

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,16 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+public class CreateOrderHandler
+    : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+{
+    public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        // TODO:
+        // Create Order entity from command object
+        // save to database
+        // return result
+
+        throw new NotImplementedException();
+    }
+}
