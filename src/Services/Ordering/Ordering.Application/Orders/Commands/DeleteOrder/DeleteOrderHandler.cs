2d7e25de386508e325663c5c5df93b8fb6e903b5
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2025-03-19 12:18:26 +0530

    nothing

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder;
+public class DeleteOrderHandler(IApplicationDbContext _dbContext)
+    : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+{
+    public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+    {
+        var orderId = OrderId.Of(command.OrderId);
+        var order = await _dbContext.Orders
+            .FindAsync([orderId], cancellationToken: cancellationToken);
+
+        if (order is null)
+            throw new OrderNotFoundException(command.OrderId);
+
+        _dbContext.Orders.Remove(order);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return new DeleteOrderResult(true);
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderHandler.cs
@@ -0,0 +1,46 @@
+﻿namespace Ordering.Application.Orders.Commands.UpdateOrder;
+public class UpdateOrderHandler(IApplicationDbContext _dbContext)
+    : ICommandHandler<UpdateOrderCommand, UpdateOrderResult>
+{
+    public async Task<UpdateOrderResult> Handle(UpdateOrderCommand command, CancellationToken cancellationToken)
+    {
+
+        var orderId = OrderId.Of(command.Order.Id);
+        var order = await _dbContext.Orders
+            .FindAsync([orderId], cancellationToken: cancellationToken);
+
+        if (order is  null)
+            throw new OrderNotFoundException(command.Order.Id);
+
+        UpdateOrderWithNewValues(order, command.Order);
+
+        _dbContext.Orders.Update(order);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return new UpdateOrderResult(true);
+    }
+
+    private void UpdateOrderWithNewValues(Order order, OrderDto orderDto)
+    {
+        var updateShippingAddress = Address
+            .Of(orderDto.ShippingAddress.FirstName, orderDto.ShippingAddress.LastName,
+            orderDto.ShippingAddress.EmailAddress, orderDto.ShippingAddress.AddressLine,
+            orderDto.ShippingAddress.Country, orderDto.ShippingAddress.State, orderDto.ShippingAddress.ZipCode);
+
+        var updateBillingAddress = Address
+            .Of(orderDto.BillingAddress.FirstName, orderDto.BillingAddress.LastName,
+            orderDto.BillingAddress.EmailAddress, orderDto.BillingAddress.AddressLine,
+            orderDto.BillingAddress.Country, orderDto.BillingAddress.State, orderDto.BillingAddress.ZipCode);
+
+        var updatePayment = Payment
+            .Of(orderDto.Payment.CardName, orderDto.Payment.CardNumber, orderDto.Payment.Expiration,
+            orderDto.Payment.Cvv, orderDto.Payment.PaymentMethod);
+
+        order.Update(
+            orderName: OrderName.Of(orderDto.OrderName),
+            shippingAddress: updateShippingAddress,
+            billingAddress: updateBillingAddress,
+            payment: updatePayment,
+            status: orderDto.Status);
+    }
+}
